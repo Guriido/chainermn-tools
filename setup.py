f84fd37b051a92bbb0cb565b6ff84d6058cabc16
Guriido <christian.hamelain@laposte.net>
2019-06-26 19:55:33 +0900

    delete unrelevant line

--- setup.py
+++ setup.py
@@ -52,7 +52,6 @@ setup(
     # # You can just specify the packages manually here if your project is
     # # simple. Or you can use find_packages().
     # packages=[
-    #     'chainermn_utils',
     # ],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
